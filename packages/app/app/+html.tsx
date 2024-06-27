4eb5a124909740bf739e05fa0d27fa9bdb5def72
Eli Mallon <eli@aquareum.tv>
2024-06-27 05:13:11 +0000

    app: placeholder icons
    See merge request aquareum-tv/aquareum!12

--- packages/app/app/+html.tsx
+++ packages/app/app/+html.tsx
@@ -10,6 +10,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <title>Aquareum</title>
 
         {/* 
           This viewport disables scaling which makes the mobile website act more like a native app.
