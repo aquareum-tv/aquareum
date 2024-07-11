54c05952327905e2c70302f06dfe1c83ff238e07
Eli Mallon <eli@aquareum.tv>
2024-07-10 21:59:38 -0700

    ok, we have some push notifications

--- js/app/app/platform/platform.mobile.tsx
+++ js/app/app/platform/platform.mobile.tsx
@@ -14,75 +14,78 @@ const checkApplicationPermission = async () => {
 };
 
 export async function initPushNotifications() {
-  const x = messaging();
-  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-    console.log("Message handled in the background!", remoteMessage);
-  });
-  await checkApplicationPermission();
-  const authorizationStatus = await x.requestPermission();
+  try {
+    const x = messaging();
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+    await checkApplicationPermission();
+    const authorizationStatus = await x.requestPermission();
 
-  let perms = "";
+    let perms = "";
 
-  if (authorizationStatus === messaging.AuthorizationStatus.AUTHORIZED) {
-    console.log("User has notification permissions enabled.");
-    perms += "authorized";
-  } else if (
-    authorizationStatus === messaging.AuthorizationStatus.PROVISIONAL
-  ) {
-    console.log("User has provisional notification permissions.");
-    perms += "provisional";
-  } else {
-    console.log("User has notification permissions disabled");
-    perms += "disabled";
-  }
-
-  (async () => {
-    if (typeof process.env.EXPO_PUBLIC_AQUAREUM_URL !== "string") {
-      console.log("process.env.EXPO_PUBLIC_AQUAREUM_URL undefined!");
-      return;
+    if (authorizationStatus === messaging.AuthorizationStatus.AUTHORIZED) {
+      console.log("User has notification permissions enabled.");
+      perms += "authorized";
+    } else if (
+      authorizationStatus === messaging.AuthorizationStatus.PROVISIONAL
+    ) {
+      console.log("User has provisional notification permissions.");
+      perms += "provisional";
+    } else {
+      console.log("User has notification permissions disabled");
+      perms += "disabled";
     }
-    try {
-      const token = await x.getToken();
-      console.log(`messaging tokennn: ${token}`);
-      const res = await fetch(
-        `${process.env.EXPO_PUBLIC_AQUAREUM_URL}/api/notification`,
-        {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
+
+    (async () => {
+      if (typeof process.env.EXPO_PUBLIC_AQUAREUM_URL !== "string") {
+        console.log("process.env.EXPO_PUBLIC_AQUAREUM_URL undefined!");
+        return;
+      }
+      try {
+        const token = await x.getToken();
+        const res = await fetch(
+          `${process.env.EXPO_PUBLIC_AQUAREUM_URL}/api/notification`,
+          {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({ token }),
           },
-          body: JSON.stringify({ token }),
-        },
-      );
-      console.log({ status: res.status });
-    } catch (e) {
-      console.log(e);
-    }
-  })();
-  // Register background handler
+        );
+        console.log({ status: res.status });
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+    // Register background handler
 
-  messaging()
-    .subscribeToTopic("live")
-    .then(() => console.log("Subscribed to live!"));
+    messaging()
+      .subscribeToTopic("live")
+      .then(() => console.log("Subscribed to live!"));
 
-  messaging().onMessage((remoteMessage) => {
-    console.log("Foreground message:", remoteMessage);
-    // Display the notification to the user
-  });
-  messaging().onNotificationOpenedApp((remoteMessage) => {
-    console.log(
-      "App opened by notification while in foreground:",
-      remoteMessage,
-    );
-    // Handle notification interaction when the app is in the foreground
-  });
-  messaging()
-    .getInitialNotification()
-    .then((remoteMessage) => {
+    messaging().onMessage((remoteMessage) => {
+      console.log("Foreground message:", remoteMessage);
+      // Display the notification to the user
+    });
+    messaging().onNotificationOpenedApp((remoteMessage) => {
       console.log(
-        "App opened by notification from closed state:",
+        "App opened by notification while in foreground:",
         remoteMessage,
       );
-      // Handle notification interaction when the app is opened from a closed state
+      // Handle notification interaction when the app is in the foreground
     });
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        console.log(
+          "App opened by notification from closed state:",
+          remoteMessage,
+        );
+        // Handle notification interaction when the app is opened from a closed state
+      });
+  } catch (e) {
+    console.log(e);
+  }
 }
