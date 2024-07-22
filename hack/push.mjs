8f7d447e31b3909b851357fe3e71d0e9b3d62962
Eli Mallon <eli@aquareum.tv>
2024-07-22 03:35:16 +0000

    push.mjs improvements
    See merge request aquareum-tv/aquareum!35

--- hack/push.mjs
+++ hack/push.mjs
@@ -9,17 +9,60 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-(async () => {
-  const res = await admin.messaging().sendMulticast({
-    tokens: process.argv.slice(2),
+// const tokens = JSON.parse(process.argv.slice(2))
+// console.log(tokens)
+// process.exit(0)
+
+// curl -H "Authorization: Bearer $AQ_ADMIN" https://aquareum.tv/api/notification
+
+const delay = (ms) => new Promise((r) => setTimeout(r, ms));
+const blast = async (body) => {
+  const tokensRes = await fetch("https://aquareum.tv/api/notification", {
+    headers: {
+      Authorization: `Bearer ${process.env.AQ_ADMIN}`,
+    },
+  });
+  const fullTokens = await tokensRes.json();
+  const tokens = fullTokens.map((t) => t.Token);
+  const notification = {
+    tokens: tokens,
     notification: {
-      title: "Basic Notification",
-      body: "This is a basic notification sent from the server!",
+      title: "🔴 @iame.li is LIVE!",
+      body: body,
       // imageUrl: "https://my-cdn.com/app-logo.png",
+    },
+    apns: {
+      headers: {
+        "apns-priority": "10",
+      },
+      payload: {
+        aps: {
+          sound: "default",
+        },
+      },
     },
     android: {
       priority: "high",
+      notification: {
+        sound: "default",
+      },
     },
-  });
+    priority: "high",
+  };
+  console.log(JSON.stringify(notification.notification, null, 2));
+  // console.log("launching in 5 sec")
+  // await delay(5000)
+  const res = await admin.messaging().sendMulticast(notification);
   console.log(JSON.stringify(res));
-})();
+};
+export default blast;
+
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const pathToThisFile = resolve(fileURLToPath(import.meta.url));
+const pathPassedToNode = resolve(process.argv[1]);
+const isThisFileBeingRunViaCLI = pathToThisFile.includes(pathPassedToNode);
+if (isThisFileBeingRunViaCLI) {
+  blast(process.argv[2]);
+}
