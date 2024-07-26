3e8fa49e30d978bba2defc0094970183f9dc3554
Eli Mallon <eli@aquareum.tv>
2024-07-26 12:54:11 -0700

    optional

--- util/kaniko-ci.sh
+++ util/kaniko-ci.sh
@@ -28,14 +28,14 @@ build() {
     --build-arg CACHED_BUILD_IMAGE="$CACHED_BUILD_IMAGE" \
     --build-arg TARGETARCH=amd64 \
     --build-arg CI_API_V4_URL=$CI_API_V4_URL \
-    --build-arg CI_COMMIT_BRANCH=$CI_COMMIT_BRANCH \
     --build-arg CI_COMMIT_SHA=$CI_COMMIT_SHA \
-    --build-arg CI_COMMIT_TAG=$CI_COMMIT_TAG \
     --build-arg CI_JOB_TOKEN=$CI_JOB_TOKEN \
     --build-arg CI_PROJECT_DIR=$CI_PROJECT_DIR \
     --build-arg CI_PROJECT_ID=$CI_PROJECT_ID \
     --build-arg CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE \
     --build-arg CI_REPOSITORY_URL=$CI_REPOSITORY_URL \
+    --build-arg CI_COMMIT_TAG=${CI_COMMIT_TAG:-} \
+    --build-arg CI_COMMIT_BRANCH=${CI_COMMIT_BRANCH:-} \
     --cache=true \
     --cache-repo "$CI_REGISTRY_IMAGE" \
     --context "$CI_PROJECT_DIR" \
