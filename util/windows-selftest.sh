3f36627369d0b9e8cc10ed686e969e8d4d3b34bc
Eli Mallon <eli@aquareum.tv>
2024-10-18 16:51:18 -0700

    maybe?

--- util/windows-selftest.sh
+++ util/windows-selftest.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 set -euo pipefail
+set -x
 
-echo "hooray!"
+curl -L -o aquareum-desktop.exe https://git.aquareum.tv/aquareum-tv/aquareum/-/package_files/2844/download
+./aquareum-desktop.exe
+cd /c/Users/admin/AppData/Local/aquareum_desktop
+cd app-*
+./Aquareum.exe -- --self-test
