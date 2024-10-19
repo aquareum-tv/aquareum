a587bb161ad465c20f527cced34d02b838abb390
Eli Mallon <eli@aquareum.tv>
2024-10-18 17:18:15 -0700

    better...

--- util/windows-selftest.sh
+++ util/windows-selftest.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 set -x
 
-curl -L -o aquareum-desktop.exe https://git.aquareum.tv/aquareum-tv/aquareum/-/package_files/2844/download
+curl -L -o aquareum-desktop.exe https://git.aquareum.tv/aquareum-tv/aquareum/-/package_files/2873/download
 ./aquareum-desktop.exe
+powershell -Command "Stop-Process -Name Aquareum"
 cd /c/Users/admin/AppData/Local/aquareum_desktop
 cd app-*
 ./Aquareum.exe -- --self-test
